f07556780dd38ba64024224e100fa03eaaac1cc9
BestTTTA <bst12546@gmail.com>
2024-10-06 00:59:59 +0700

    update component home page6

--- src/app/components/Home/components/Page6.tsx
+++ src/app/components/Home/components/Page6.tsx
@@ -1,11 +1,11 @@
-import {FC} from 'react'
+import { FC } from "react";
 
 const Page6: FC = () => {
-    return (
-        <main>
-            <h1>Welcome to page6</h1>
-        </main>
-    )
-}
+  return (
+    <main>
+      <h1>page6</h1>
+    </main>
+  );
+};
 
 export default Page6;
