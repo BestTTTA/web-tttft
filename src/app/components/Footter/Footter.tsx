d2f4e7d7f3c5e38b8e116b7c1d63ea95297785dd
BestTTTA <bst12546@gmail.com>
2024-10-06 23:53:35 +0700

    update image

--- src/app/components/Footter/Footter.tsx
+++ src/app/components/Footter/Footter.tsx
@@ -1,12 +1,15 @@
 import { FC } from "react";
 
-
 const Footter: FC = () => {
-    return (
-        <div className="h-[270px] w-full bg-black">
-
-        </div>
-    )
-}
+  return (
+    <div className="flex h-[270px] w-full bg-black">
+      <div className="flex w-[20%] border justify-center items-center p-6">
+        <img src="/tttft_logo_white.png" alt="Thetigerteamfoundation Technology" className="w-auto h-auto" />
+      </div>
+      <div className="flex w-[60%] border"></div>
+      <div className="flex w-[20%] border"></div>
+    </div>
+  );
+};
 
 export default Footter;
